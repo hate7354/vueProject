0edd01bf57b6d5046010c52e493b21a5203b6eba
Jin Woong Lee <80955533+hate7354@users.noreply.github.com>
2022-07-18 16:37:29 +0900

    first commit

---
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,21 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ");
+		System.out.println("하히ㅏㅣ22222222222222222222222");
+	}
+
+}
