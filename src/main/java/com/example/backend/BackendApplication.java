88d2ea78fd85ff1b25790f921aec86b0a54575c8
Jin Woong Lee <80955533+hate7354@users.noreply.github.com>
2022-07-19 15:53:18 +0900

    ㅎㅇ

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,9 +2,11 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -16,6 +18,11 @@ public class BackendApplication {
 		System.out.println("하히ㅏㅣ");
 		System.out.println("하히ㅏㅣ");
 		System.out.println("하히ㅏㅣ22222222222222222222222");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(BackendApplication.class);
 	}
 
 }
